17cfda2e6972720a64e976525dc68206fe131f5b
Rikki Tooley <hello@rikk.it>
2014-09-16 15:59:35 +0100

    Starting to add unit tests. Goal: 100%.
    "There's always gonna be another mountain
    I'm always gonna wanna make it move
    Always gonna be an uphill battle
    Sometimes I'm gonna have to lose
    Ain't about how fast I get there
    Ain't about what's waitin' on the other side
    It's the climb"
    - Miley Cyrus 2k9

---
+++ src/IF.Common.Tests/UI/ConverterTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Windows.UI.Xaml;
+using IF.Common.Metro.UI.Converters;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace IF.Common.Tests.UI
+{
+    [TestClass]
+    public class ConverterTests
+    {
+        [TestMethod]
+        public void BooleanToVisibility()
+        {
+            var converter = new BooleanToVisibility();
+            object answer;
+
+            answer = converter.Convert(true, null, null, null);
+            Assert.IsTrue(answer is Visibility);
+            Assert.IsTrue((Visibility)answer == Visibility.Visible);
+
+            answer = converter.Convert(false, null, null, null);
+            Assert.IsTrue(answer is Visibility);
+            Assert.IsTrue((Visibility)answer == Visibility.Collapsed);
+        }
+
+        [TestMethod]
+        public void BooleanToVisibilityReversed()
+        {
+            var converter = new BooleanToVisibility() { Reverse = true };
+            object answer;
+
+            answer = converter.Convert(true, null, null, null);
+            Assert.IsTrue(answer is Visibility);
+            Assert.IsTrue((Visibility)answer == Visibility.Collapsed);
+
+            answer = converter.Convert(false, null, null, null);
+            Assert.IsTrue(answer is Visibility);
+            Assert.IsTrue((Visibility)answer == Visibility.Visible);
+        }
+
+        [TestMethod]
+        public void BooleanToVisibilityConvertBack()
+        {
+            var converter = new BooleanToVisibility();
+
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                converter.ConvertBack(true, null, null, null);
+            });
+        }
+    }
+}
