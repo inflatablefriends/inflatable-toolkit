c3c48e05013834e3d1c233f00d727127018c20b9
Rikki Tooley <rikki@inflatablefriends.co.uk>
2015-02-28 22:42:48 +0000

    Implementation of IGrouping<T,U)

---
+++ src/IF.Common.Metro/Framework/Group.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+namespace IF.Common.Metro.Framework
+{
+    public class Group<T, U> : IGrouping<T, U>
+    {
+        private IEnumerable<U> _items;
+
+        public Group(IEnumerable<U> items, T key)
+        {
+            _items = items;
+            Key = key;
+        }
+
+        public T Key { get; private set; }
+
+        public IEnumerator<U> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
